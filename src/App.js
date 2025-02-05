71fba3368a47f01a91cc1a3e2f8fc3adbaaadf86
MikeDev <mikestefan1926@gmail.com>
2025-02-05 20:18:44 +0200

    CampsiteDetailPage with useParams

--- src/App.js
+++ src/App.js
@@ -5,15 +5,20 @@ import './App.css';
 import Header from './components/Header.js';
 import Footer from './components/Footer';
 import CampsitesDirectoryPage from './pages/CampsitesDirectoryPage.js';
+import CampsiteDetailPage from './pages/CampsiteDetailPage';
 
 function App() {
     return (
         <div className='App'>
             <Header />
-            <Routes>
+            <Routes> 
                 <Route path='/' element={<HomePage />} />
                 <Route path='contact' element={<ContactPage />} />
                 <Route path='directory' element={<CampsitesDirectoryPage />} />
+                <Route
+                    path='directory/:campsiteId'
+                    element={<CampsiteDetailPage />}
+                />
             </Routes>
             <Footer />
         </div>
